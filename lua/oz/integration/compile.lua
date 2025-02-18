323b93e280ceddc9e1a1dd7080e4218e22127d73
suvasanketrout <rout.suvasanket2@icloud.com>
2025-02-18 10:32:20 +0530

    compile mode integration now works other way too

--- lua/oz/integration/compile.lua
+++ lua/oz/integration/compile.lua
@@ -1,15 +1,25 @@
 local M = {}
 local k = require("oz.mappings")
+local t = require("oz.term")
 
 function M.compile_init()
 	vim.api.nvim_create_autocmd("FileType", {
-		pattern = "compilation",
+		pattern = { "compilation", "oz_term" },
 		callback = function(event)
-			vim.keymap.set("n", "t", function()
-				vim.cmd("CompileInterrupt")
-				vim.cmd("close")
-				k.cmd_func("Term")
-			end, { buffer = event.buf, silent = true })
+            -- compilation to term
+			if vim.bo.ft == "compilation" then
+				vim.keymap.set("n", "t", function()
+					vim.cmd("CompileInterrupt")
+					vim.cmd("close")
+					k.cmd_func("Term")
+				end, { buffer = event.buf, silent = true })
+            -- term to compilation
+			elseif vim.bo.ft == "oz_term" then
+                vim.keymap.set("n", "t", function()
+                    t.close_term()
+					k.cmd_func("Compile")
+                end, { buffer = event.buf, silent = true })
+			end
 		end,
 	})
 end
