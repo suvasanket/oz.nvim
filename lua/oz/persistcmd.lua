1814f8e1b3d39842557628b638fd18a7f8e0351d
suvasanketrout <rout.suvasanket2@icloud.com>
2025-02-19 19:38:41 +0530

    double quote issue resolved

--- lua/oz/persistcmd.lua
+++ lua/oz/persistcmd.lua
@@ -10,13 +10,14 @@ local oil_json = data_dir .. "/oz/oilcmd.json"
 -- generate the command to get from json file
 local function gen_setcmd(key, value, json_path)
 	local data_writecmd = [[
-    mkdir -p "{data_dir}" && [ -f "{json_path}" ] || echo '{}' > "{json_path}" && jq '. + {"{key}": "{value}"}' "{json_path}" > temp.json_path && mv temp.json_path "{json_path}"
+    mkdir -p "{data_dir}" && [ -f "{json_path}" ] || echo '{}' > "{json_path}" && jq '. + {"{key}": "{value}"}' "{json_path}" > oz_temp.json && mv oz_temp.json "{json_path}"
     ]]
-	return data_writecmd
+	data_writecmd = data_writecmd
+		:gsub("{json_path}", json_path)
 		:gsub("{data_dir}", data_dir .. "/oz")
 		:gsub("{key}", key)
 		:gsub("{value}", value)
-		:gsub("{json_path}", json_path)
+	return data_writecmd
 end
 
 -- generate the command to set data to json file
@@ -27,29 +28,37 @@ local function gen_getcmd(key, json_path)
 	return data_readcmd:gsub("{key}", key):gsub("{json_path}", json_path)
 end
 
+-- remove oz_temp.json if error
+local function remove_tempjson()
+	util.ShellCmd('[ -f "oz_temp.json" ] && rm oz_temp.json', function()
+		print("oz(error): temp files removed")
+	end, nil)
+end
+
 -- set file cmd
 function M.setpersistCMD(project_path, file, ft, cmd)
 	local key = [[{project_path}${ft}]]
 	key = key:gsub("{project_path}", project_path):gsub("{ft}", ft)
-    if cmd:find(file) then
-        cmd = cmd:gsub(file, "{filename}")
-    end
+	if cmd:find(file) then
+		cmd = cmd:gsub(file, "{filename}")
+	end
 
 	util.ShellCmd(gen_setcmd(key, cmd, data_json), nil, function()
 		util.Notify("error occured saving command", "error", "Error")
+		remove_tempjson()
 	end)
 end
 
 -- get file cmd
 function M.getpersistCMD(project_path, file, ft)
-    local key = [[{project_path}${ft}]]
-    key = key:gsub("{project_path}", project_path):gsub("{ft}", ft)
+	local key = [[{project_path}${ft}]]
+	key = key:gsub("{project_path}", project_path):gsub("{ft}", ft)
 
 	local out = util.ShellOutput(gen_getcmd(key:gsub("/", "\\/"), data_json))
 	if out and out ~= "null" then
-        if out:find("{filename}") then
-            out = out:gsub("{filename}", file)
-        end
+		if out:find("{filename}") then
+			out = out:gsub("{filename}", file)
+		end
 		return out
 	else
 		return nil
@@ -67,6 +76,7 @@ function M.setftCMD(file, ft, cmd)
 
 	util.ShellCmd({ "sh", "-c", gen_setcmd(ft, cmd, ft_json) }, nil, function()
 		util.Notify("error occured saving ft command", "error", "Error")
+		remove_tempjson()
 	end)
 end
 
@@ -92,6 +102,7 @@ end
 function M.setTermBcmd(current_file, cmd)
 	util.ShellCmd({ "sh", "-c", gen_setcmd(current_file, cmd, termbang_json) }, nil, function()
 		util.Notify("error occured saving Term! command", "error", "Error")
+		remove_tempjson()
 	end)
 end
 
@@ -108,8 +119,11 @@ end
 
 -- set oil cmd
 function M.setoilcmd(cwd, cmd)
+	cmd = cmd:gsub('"', '\\"')
+
 	util.ShellCmd({ "sh", "-c", gen_setcmd(cwd, cmd, oil_json) }, nil, function()
 		util.Notify("error occured saving oil command", "error", "Error")
+		remove_tempjson()
 	end)
 end
 
